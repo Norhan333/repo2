542d237d8a93712da8b55c0c7bf32035579dcb72
Norhan333 <nourhanallam02@gmail.com>
2025-08-08 12:45:56 +0300

    ss

--- obj/Debug/net9.0/test2.AssemblyInfo.cs
+++ obj/Debug/net9.0/test2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6542e7a5a4b2fc4c58be43d9553c61f3e9e8114")]
 [assembly: System.Reflection.AssemblyProductAttribute("test2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
